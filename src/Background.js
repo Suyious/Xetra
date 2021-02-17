3c593e13362ec4943cd37f29be7f21c9473d66c7
Suyash <suyu162001@gmail.com>
2021-02-18 01:53:39 +0530

    123

--- src/Background.js
+++ src/Background.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Backimage from './media/Photobac.png'
+import React from "react";
+import Backimage from "./media/BackgroundPic.png";
 
-function Background({scrolled}) {
-    return (
-        <div className="background">
-            <img className="heroback" src={Backimage} alt=""/>
-            <div className={`ribbon ${scrolled?"vanish":"appear"}`}></div>
-        </div>
-        
-    )
+function Background({ scrolled }) {
+  return (
+    <div className="background">
+      <img className="heroback" src={Backimage} alt="" />
+      <div className={`ribbon ${scrolled ? "vanish" : "appear"}`}></div>
+    </div>
+  );
 }
 
-export default Background
+export default Background;
